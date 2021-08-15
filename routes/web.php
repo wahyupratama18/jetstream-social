1de41484dc89e99da60e4bb5f89b8b59fb96a344
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2021-08-15 21:35:56 +0700

    Remove controllers namespace at route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\{
-    FacebookController,
-    GithubController,
-    GoogleController,
-    LinkedinController,
-    TwitterController
-};
 use Illuminate\Support\Facades\Route;
 
 /*
